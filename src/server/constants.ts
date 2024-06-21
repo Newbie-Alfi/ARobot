b20f220c7214c1305eb219d5108ab5e2ff141e6c
Newbie-Alfi <aleksandr20021008@gmail.com>
2024-06-21 10:26:19 +0300

    рефакторинг

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -2,6 +2,14 @@ export const HARDCODED_INSTRUMENTS = [
   { figi: "BBG004731354", description: "Роснефть" },
   { figi: "BBG008F2T3T2", description: "РУСАЛ" },
   { figi: "BBG004S68829", description: "Татнефть" },
-  { figi: "BBG000BN56Q9", description: "Детский Мир" },
   { figi: "BBG004730N88", description: "Сбер" },
+  { figi: "BBG004TC84Z8", description: "Трубная Металлургическая Компания" },
+  { figi: "TCS00A0ZZBC2", description: "Софтлайн" },
+  { figi: "BBG000VKG4R5", description: "Россети Урал" },
+  { figi: "BBG000C7P5M7", description: "МРСК Юга" },
+  {
+    figi: "BBG002B2J5X0",
+    description: "Саратовский НПЗ - акции привилегированные",
+  },
+  { figi: "BBG00475K6C3", description: "Северсталь" },
 ];
